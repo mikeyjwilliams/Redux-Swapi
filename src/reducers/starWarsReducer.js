78033505154e4d5bdfa694d7a5a50c96243561ac
Michael Williams <manicdesigner@gmail.com>
2019-07-17 13:21:38 -0400

    feat(starwarsreducer) + imports + initialState bool and null + started case

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,13 @@
-import /* we need our action types here*/ "../actions";
+import {
+  /* we need our action types here*/
+  GET_CHARACTERS_START,
+  GET_CHARACTERS_SUCCESS,
+  GET_CHARACTERS_FAILED
+} from '../actions';
 const initialState = {
-  characters: []
+  characters: [],
+  getCharacters: false,
+  errorMessage: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +15,9 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case GET_CHARACTERS_START:
+      return {};
+
     default:
       return state;
   }
