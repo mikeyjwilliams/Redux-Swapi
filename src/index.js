554d7f339c7719790dab48d209906491f3176a88
Michael Williams <manicdesigner@gmail.com>
2019-07-17 10:47:53 -0400

    middleware(index.js) + redux-devtool-ext + thunk + logger

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
@@ -15,7 +15,14 @@ import thunk from 'redux-thunk';
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+
   /* applyMiddleware goes here */
 );
 
