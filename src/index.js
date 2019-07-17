499ac342e6ef4af677bb8871a1b902498c5c19c8
Michael Williams <manicdesigner@gmail.com>
2019-07-16 21:40:55 -0400

    packages and starting the index source

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
@@ -20,5 +23,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
