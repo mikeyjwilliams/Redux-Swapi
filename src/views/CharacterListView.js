69fc00f7ef2dfd061f7c3f17e406d7c8d91fba4c
Michael Williams <manicdesigner@gmail.com>
2019-07-19 12:43:02 -0400

    feat(characterListView) + import action fetchCharacter + CDM fetcharacter() + return loading if props.fetching + mapStateToProps + fixed w/ charsReducer between state & objects + action creator in connect

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
+import { CharacterList } from '../components';
 // import actions
+import { fetchCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.fetchCharacters();
   }
 
   render() {
+    console.log(this.props.characters);
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h2>Loading...</h2>;
     }
     return (
       <div className="CharactersList_wrapper">
@@ -24,12 +23,16 @@ class CharacterListView extends React.Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  console.log('state', state);
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching
+  };
+};
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps /* mapStateToProps replaces null here */,
+  { fetchCharacters }
 )(CharacterListView);
